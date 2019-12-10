563ca297976d15b16fa0c41f17a5d1c240b7be4d
felix yanwei wang <felixw@mit.edu>
2019-12-09 22:03:32 -0500

    add README; train.py

---
+++ README.md
@@ -0,0 +1,13 @@
+# Data description
+
+image_pos: only position data
+
+image_0-7: arm no reset
+
+image_10-17: arm reset but occasionally unreachable
+
+image_20-27, 30-37: arm reset always reachable
+
+image_28-29: long range data; 28 all, 29 first 1k data no collision
+
+image_38-39: even longer range data; 38 all, 39 first 1k data no collision
---
+++ train.py
@@ -0,0 +1,294 @@
+from __future__ import print_function, division
+import os
+import time
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils, models
+
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+plt.ion()   # interactive mode
+np.set_printoptions(precision=3, suppress=True)
+torch.set_printoptions(precision=3, sci_mode=False)
+
+# label index in the poke data array (23, 1)
+#index 0 is the poke index corresponding to starting image
+stx, sty, edx, edy = 1, 2, 3, 4 # ee pos of start and end poke
+obx, oby, qt1, qt2, qt3, qt4 = 5, 6, 7, 8, 9, 10 # obj pose before poke
+js1, js2, js3, js4, js5, js6 = 11, 12, 13, 14, 15, 16 # jpos before poke
+je1, je2, je3, je4, je5, je6 = 17, 18, 19, 20, 21, 22 #jpos after poke
+
+class PokeDataset(Dataset):
+
+    def __init__(self, dirname, size=None, transform=None):
+        self.dirname = dirname
+        self.data = np.loadtxt(dirname + '.txt')
+        self.transform = transform
+        self.size = size
+
+    def __len__(self):
+        if self.size: # in cases where I want to define size
+            return self.size
+        else:
+            return len(self.data)
+
+    def __getitem__(self, idx):
+        '''idx should be a single value, not list'''
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        if idx == -1:
+            idx = self.__len__() - 1 # a hack to prevent accessing img1 out of range
+        if idx > self.__len__()-1:
+            raise ValueError('idx must be smaller than the len-1\
+                                in order to get two images')
+
+        img1_name = os.path.join(self.dirname, str(int(self.data[idx, 0]))) + '.png'
+        # fix the last idx using identity poke (i.e. zero poke)
+        if idx == self.__len__()-1:
+            img2_name = img1_name
+            poke = np.array([0.0, 0.0, 0.0, 0.0], dtype='float32')
+        else:
+            img2_name = os.path.join(self.dirname, str(int(self.data[idx, 0])+1)) + '.png'
+            poke = np.float32(self.data[idx, stx:edy+1])
+
+        img1 = imread(img1_name)
+        img2 = imread(img2_name)
+
+#         imgx = img1.shape[0]
+#         imgy = img1.shape[1]
+#         coordconv1 = np.array([list(range(imgy)),] * imgx).reshape(imgx, imgy, -1).astype('float32')
+#         coordconv2 = np.array([list(range(imgx)),] * imgy).T.reshape(imgx, imgy, -1).astype('float32')
+#         img1 = np.concatenate([img1, coordconv1, coordconv2], axis=2)
+#         img2 = np.concatenate([img2, coordconv1, coordconv2], axis=2)
+
+        if self.transform:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+#         sample = {'img1': img1, 'img2': img2, 'poke': poke}
+#         img = np.vstack((img1, img2))
+#         pokevec = np.array([poke[2]-poke[0], poke[3]-poke[1]])
+
+        sample = {'img': torch.stack((img1, img2)), 'poke':torch.from_numpy(poke)}
+
+
+        return sample
+
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class Siamese(nn.Module):
+    def __init__(self, use_init):
+        super(Siamese, self).__init__()
+        self.base = models.resnet18(pretrained=use_init)
+#         self.base.conv1 = nn.Conv2d(5, 64, kernel_size=(7,7), stride=(2,2), padding=(3,3), bias=False)
+        self.base.fc = Identity()
+        self.fc = nn.Sequential(nn.Linear(in_features=2*512, out_features=4))
+
+    def forward(self, x):
+        '''x of size (batch, 2, C, H, W)'''
+#         feat_1, feat_2 = torch.split(x, 1, 1)
+#         feat_1, feat_2 = torch.squeeze(feat_1, 1), torch.squeeze(feat_2, 1)
+#         feat_1 = self.base(feat_1)
+#         feat_2 = self.base(feat_2)
+
+#         feat = torch.cat([feat_1, feat_2], 1)
+        x = x.reshape((-1, x.size(2), x.size(3), x.size(4)))
+        stacked_feat = self.base(x)
+        feat_1, feat_2 = stacked_feat[:x.size(0)//2], stacked_feat[x.size(0)//2:]
+        feat = torch.cat([feat_1, feat_2], 1)
+        out = self.fc(feat)
+        return out
+
+
+def train(use_4_gpus=False, lr=0.1, bsize=256, nwork=16, num_epochs=30, use_init=True):
+    # model
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    model = Siamese(use_init=use_init)
+    if use_4_gpus:
+        if torch.cuda.device_count() > 1:
+            print('Use', torch.cuda.device_count(), "GPUs")
+            model = nn.DataParallel(model)
+    model = model.to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+    criterion = nn.MSELoss()
+
+    # data folders
+    train_dirs = ['image_21', 'image_22','image_23', 'image_24', 'image_25', 'image_26']#,
+    #               'image_27', 'image_30', 'image_31', 'image_32']
+    valid_dirs = ['image_20', 'image_28', 'image_38']
+    data_dirs = {'train': train_dirs, 'val': valid_dirs}
+    train_num_per_dir = 5000
+    valid_num_per_dir = 1000
+
+    # dataloading
+    if use_init:
+        data_transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])])
+    else:
+        data_transforms = transforms.ToTensor()
+    train_loaders = {}
+    valid_loaders = {}
+
+    for data_dir in train_dirs:
+        pokeset = PokeDataset(data_dir, size=train_num_per_dir, transform=data_transforms)
+        loader = DataLoader(pokeset, batch_size=bsize, shuffle=True, num_workers=nwork)
+        train_loaders[data_dir] = loader
+
+    for data_dir in valid_dirs:
+        pokeset = PokeDataset(data_dir, size=valid_num_per_dir, transform=data_transforms)
+        loader = DataLoader(pokeset, batch_size=bsize, shuffle=False, num_workers=nwork)
+        valid_loaders[data_dir] = loader
+
+    dataloaders = {'train': train_loaders, 'val': valid_loaders}
+
+
+    # training
+    since = time.time()
+    last_time = since
+    valid_loss_history = []
+    train_loss_history = []
+    best_model_wts = copy.deepcopy(model.state_dict())
+    lowest_loss = 10 # some big number
+    running_losses = {}
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs-1))
+        print('-' * 10)
+        print('train sets ', train_dirs)
+        print('batch size ', bsize)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+                running_losses[phase] = 0.0
+            else:
+                model.eval()
+                running_losses[phase] = {}
+                for data_dir in valid_dirs:
+                    running_losses[phase][data_dir] = 0.0
+
+            # iterate over data
+            for data_dir in data_dirs[phase]:
+                for batched_sample in dataloaders[phase][data_dir]:
+                    inputs = batched_sample['img']
+                    labels = batched_sample['poke']
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+    #                 print(inputs.min(), inputs.max(), inputs.size())
+    #                 print(labels.size(), labels.min(), labels.max())
+    #                 assert False
+                    # zero parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward pass
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+
+    #                     loss = torch.abs(outputs - labels).mean()
+                        loss = criterion(outputs, labels)
+    #                     print(phase, loss.item())
+
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    if phase == 'train':
+                        running_losses[phase] += loss.item() * inputs.size(0)
+                    else:
+                        running_losses[phase][data_dir] += loss.item()*inputs.size(0)
+
+
+
+        # print losses
+        total_val_loss = 0.0
+        for data_dir in valid_dirs:
+            ave_val_loss = running_losses['val'][data_dir] / valid_num_per_dir
+            total_val_loss += ave_val_loss
+            print('Val {} loss: {:.4f}'.format(data_dir, ave_val_loss))
+            valid_loss_history.append(ave_val_loss)
+        if total_val_loss < lowest_loss:
+            lowest_loss = total_val_loss
+            best_model_wts = copy.deepcopy(model.state_dict())
+            print('updating best weights')
+
+        epoch_loss = running_losses['train'] / (len(train_dirs)*train_num_per_dir)
+        train_loss_history.append(epoch_loss)
+        print('Loss: {:.4f}'.format(epoch_loss))
+
+        # print time
+        curr_time = time.time()
+        epoch_time = curr_time - last_time
+        print('Epoch time: {:.0f}m {:.0f}s'.format(epoch_time // 60, epoch_time % 60))
+        time_elapsed = curr_time - since
+        print('Training so far takes in {:.0f}m {:.0f}s'.format(time_elapsed // 60,
+                                                            time_elapsed % 60))
+        last_time = curr_time
+        print()
+
+    # save best model weights
+    torch.save(best_model_wts, 'weights/tmp.pth.tar')
+
+
+def record_loss(filename):
+    with open(filename, 'a') as f:
+    f.write('image_20 ')
+    for item in vl1:
+        f.write('%s ' % item)
+        f.write('\n')
+    f.write('image_28 ')
+    for item in vl2:
+        f.write('%s' % item)
+        f.write('\n')
+    f.write('image_38 ')
+    for item in vl3:
+        f.write('%s' % item)
+        f.write('\n')
+    f.write('training ')
+    for item in train_loss_history:
+        f.write('%s' % item)
+        f.write('\n')
+
+def make_pred(datafolder, savepath):
+    import random
+    query = random.sample(range(0, 1000), 50)
+    print(query)
+
+    pokeset = PokeDataset(datafolder, transform=data_transforms)
+    pred_pokes = []
+    true_pokes = []
+    model.eval()
+    for i in query:
+        sample = pokeset[i]
+        img, poke = sample['img'], sample['poke']
+        img = img.expand([1, -1, -1, -1, -1])
+        img = img.to(device)
+        with torch.no_grad():
+            pred_poke = model(img)
+        pred_pokes.append(pred_poke.cpu().numpy())
+        true_pokes.append(poke)
+
+    pred_pokes = np.squeeze(np.stack(pred_pokes, axis=0))
+    true_pokes = np.stack(true_pokes, axis=0)
+    pokes = np.hstack([np.array(query).reshape((-1, 1)), pred_pokes, true_pokes])
+    np.savetxt(savepath, pokes)
+
+
+from IPython import embed()
+
+embed()
