aefc64cb07af9ce722b3cbe55b90e66acf05fe27
felix yanwei wang <felixw@mit.edu>
2020-01-27 14:13:11 -0500

    added code for prediction in the image space

--- gen_data.py
+++ gen_data.py
@@ -2,8 +2,10 @@ import os
 import argparse
 import time
 import cv2
+import random
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.image import imread
 import airobot as ar
 from airobot import Robot
 from airobot.utils.common import euler2quat
@@ -26,7 +28,7 @@ home = [0.6, 0, rest_height]
 table_x = 0.59 # task space x ~ (0.4, 0.75); y ~ (-0.3, 0.3)
 table_y = 0
 table_z = 0.60
-box_z = 1.0
+box_z = 1
 box_pos = [table_x, table_y, box_z]
 box_size = 0.02
 workspace_max_x = 0.8 - 0.05
@@ -71,14 +73,14 @@ def main(ifRender=False):
         rgb, depth = robot.cam.get_images(get_rgb=True, get_depth=True)
         # crop the rgb
         img = rgb[40:360, 0:640]
-        dep = depth[40:360, 0:640]
+        # dep = depth[40:360, 0:640]
         # low pass filter : Gaussian blur
         # blurred_img = cv2.GaussianBlur(img.astype('float32'), (5, 5), 0)
         small_img = cv2.resize(img.astype('float32'), dsize=(200, 100),
                     interpolation=cv2.INTER_CUBIC) # numpy array dtype numpy int64 by default
-        small_dep = cv2.resize(dep.astype('float32'), dsize=(200, 100),
-                    interpolation=cv2.INTER_CUBIC) # numpy array dtype numpy int64 by default
-        return small_img, small_dep
+        # small_dep = cv2.resize(dep.astype('float32'), dsize=(200, 100),
+        #             interpolation=cv2.INTER_CUBIC) # numpy array dtype numpy int64 by default
+        return small_img, depth
 
     # test run
     def test():
@@ -208,7 +210,8 @@ def main(ifRender=False):
                     start_r, start_c, end_r, end_c, obj_r, obj_c))
             cv2.imwrite(save_dir + '/' + str(index) +'.png',
                         cv2.cvtColor(curr_img, cv2.COLOR_RGB2BGR))
-            cv2.imwrite(save_dir + '_depth/' + str(index) +'.png', curr_dep)
+            # cv2.imwrite(save_dir + '_depth/' + str(index) +'.png', curr_dep)
+            np.save(save_dir + '_depth/' + str(index), curr_dep)
             curr_img = next_img
             curr_dep = next_dep
             if index % 1000 == 0:
@@ -237,16 +240,26 @@ def main(ifRender=False):
             print(end_jpos)
             time.sleep(1)
 
-    def eval_poke(ground_truth, img_idx, poke):
+    def eval_poke(tag, ground_truth, img_idx, poke):
         # ground_truth is the entire matrix from 'image_xx.txt' file
         # img_idx is image index, first column of ground_truth
-        # poke is the predicted 4 vector
+        # poke is the predicted 4 vector for world, 8 for pixel and joint
+        # second half of the 8 vector are world coordinates computed from gt pixel and joint values
+
+        # label index in the poke data array (29, 1)
+        # index 0 is the poke index corresponding to starting image
+        stx, sty, edx, edy = 1, 2, 3, 4 # ee pos of start and end poke
+        obx, oby, qt1, qt2, qt3, qt4 = 5, 6, 7, 8, 9, 10 # obj pose before poke
+        js1, js2, js3, js4, js5, js6 = 11, 12, 13, 14, 15, 16 # jpos before poke
+        je1, je2, je3, je4, je5, je6 = 17, 18, 19, 20, 21, 22 # jpos after poke
+        sr, stc, edr, edc, obr, obc = 23, 24, 25, 26, 27, 28 # row and col locations in image
+
         gt = ground_truth[img_idx:img_idx+2]
         tgt_posi = gt[1, 5:8]
-        tgt_posi[-1] = min_height
+        tgt_posi[-1] = box_z
         tgt_quat = gt[1, 7:11]
         init_posi = gt[0, 5:8]
-        init_posi[-1] = min_height
+        init_posi[-1] = box_z
         init_quat = gt[0, 7:11]
         gt_poke = gt[0, 1:5]
         go_home()
@@ -270,6 +283,7 @@ def main(ifRender=False):
         # go_home()
         # _, _ = get_img()
         # reset_body(box_id, init_posi, init_quat)
+        # poke = ground_truth[img_idx, 1:5]
         robot.arm.move_ee_xyz([poke[0]-home[0], poke[1]-home[1], 0], 0.015)
         robot.arm.move_ee_xyz([0, 0, min_height-rest_height], 0.015)
         robot.arm.move_ee_xyz([poke[2]-poke[0], poke[3]-poke[1], 0], 0.015)
@@ -283,14 +297,109 @@ def main(ifRender=False):
         remove_body(box_id2)
         return dist
 
-    def calc_dist(gtfile, pdfile):
+    def calc_dist(gtfile, pdfile, tag, test_num=50):
         # this function generate 50 visualization sequence
         true = np.loadtxt(gtfile)
         pred = np.loadtxt(pdfile)
         accu_dist = 0.0
-        for i in range(50):
-            accu_dist += eval_poke(true, int(pred[i][0]), pred[i][1:5])
-        return accu_dist / 50.0
+        # query = random.sample(range(0, pred.shape[0]), test_num)
+        total = pred.shape[0]
+        query = range(total)[::total/test_num]
+        for i in query:
+            img_idx = int(pred[i][0])
+            if tag == 'world':
+                poke = pred[i][1:5]
+            elif tag == 'pixel':
+                rows = np.array([pred[i, 1], pred[i, 3], pred[i, 7], pred[i, 9]])
+                cols = np.array([pred[i, 2], pred[i, 4], pred[i, 8], pred[i, 10]])
+                rows = ((rows*3.2 + 40) + 0.5).astype(int)
+                cols = ((cols*3.2) + 0.5).astype(int)
+                depth_file = gtfile[:-4] + '_depth/' + str(img_idx) + '.npy'
+                depth_im = np.load(depth_file)
+                pokes_3d = get_pix_3dpt(depth_im, rows, cols)
+                poke = pokes_3d[:, :2].flatten()
+            else:
+                raise Exception("experiment_tag has to be 'world', 'joint', or 'pixel'")
+
+            accu_dist += eval_poke(tag, true, img_idx, poke)
+        return accu_dist / len(query)
+
+
+    def get_pix_3dpt(depth_im, rs, cs, in_world=True, filter_depth=False,
+                     k=1, ktype='median', depth_min=None, depth_max=None):
+
+        if not isinstance(rs, int) and not isinstance(rs, list) and \
+                not isinstance(rs, np.ndarray):
+            raise TypeError('rs should be an int, a list or a numpy array')
+        if not isinstance(cs, int) and not isinstance(cs, list) and \
+                not isinstance(cs, np.ndarray):
+            raise TypeError('cs should be an int, a list or a numpy array')
+        if isinstance(rs, int):
+            rs = [rs]
+        if isinstance(cs, int):
+            cs = [cs]
+        if isinstance(rs, np.ndarray):
+            rs = rs.flatten()
+        if isinstance(cs, np.ndarray):
+            cs = cs.flatten()
+        if not (isinstance(k, int) and (k % 2) == 1):
+            raise TypeError('k should be a positive odd integer.')
+        # _, depth_im = self.get_images(get_rgb=False, get_depth=True)
+        if k == 1:
+            depth_im = depth_im[rs, cs]
+        else:
+            depth_im_list = []
+            if ktype == 'min':
+                ktype_func = np.min
+            elif ktype == 'max':
+                ktype_func = np.max
+            elif ktype == 'median':
+                ktype_func = np.median
+            elif ktype == 'mean':
+                ktype_func = np.mean
+            else:
+                raise TypeError('Unsupported ktype:[%s]' % ktype)
+            for r, c in zip(rs, cs):
+                s = k // 2
+                rmin = max(0, r - s)
+                rmax = min(self.img_height, r + s + 1)
+                cmin = max(0, c - s)
+                cmax = min(self.img_width, c + s + 1)
+                depth_im_list.append(ktype_func(depth_im[rmin:rmax,
+                                                cmin:cmax]))
+            depth_im = np.array(depth_im_list)
+
+        depth = depth_im.reshape(-1) * 1 #self.depth_scale
+        img_pixs = np.stack((rs, cs)).reshape(2, -1)
+        img_pixs[[0, 1], :] = img_pixs[[1, 0], :]
+        depth_min = depth_min if depth_min else 0.01 #self.depth_min
+        depth_max = depth_max if depth_max else 10 #self.depth_max
+        if filter_depth:
+            valid = depth > depth_min
+            valid = np.logical_and(valid,
+                                   depth < depth_max)
+            depth = depth[:, valid]
+            img_pixs = img_pixs[:, valid]
+
+        get_img() # to set up camera matrices
+        cam_int_mat_inv = robot.cam.cam_int_mat_inv
+        cam_ext_mat = robot.cam.cam_ext_mat
+        uv_one = np.concatenate((img_pixs,
+                                 np.ones((1, img_pixs.shape[1]))))
+        uv_one_in_cam = np.dot(cam_int_mat_inv, uv_one)
+        pts_in_cam = np.multiply(uv_one_in_cam, depth)
+        if in_world:
+            if cam_ext_mat is None:
+                raise ValueError('Please call set_cam_ext() first to set up'
+                                 ' the camera extrinsic matrix')
+            pts_in_cam = np.concatenate((pts_in_cam,
+                                         np.ones((1, pts_in_cam.shape[1]))),
+                                        axis=0)
+            pts_in_world = np.dot(cam_ext_mat, pts_in_cam)
+            pts_in_world = pts_in_world[:3, :].T
+            return pts_in_world
+        else:
+            return pts_in_cam.T
 
 
     from IPython import embed
